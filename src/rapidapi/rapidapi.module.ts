f2bf27e142fe7c28d9db9796ff48484efe295023
David Fradel <david.fradel@gmail.com>
2023-12-16 16:47:48 +0100

    feat: replace the internal cronjob by external cronjob

--- src/rapidapi/rapidapi.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RapidAPIService } from './rapidapi.service';
-
-@Module({
-  providers: [RapidAPIService],
-  exports: [RapidAPIService],
-})
-export class RapidAPIModule {}
